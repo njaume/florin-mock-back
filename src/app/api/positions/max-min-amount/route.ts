b69ba1bbdf9b8f407590995e60023fa2b770db39
Nahuel Jaume <nahueljaume@gmail.com>
2025-04-29 18:20:38 -0300

    max min amount changed

--- src/app/api/positions/max-min-amount/route.ts
+++ src/app/api/positions/max-min-amount/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   // En un entorno real, esto vendría de una configuración o servicio externo
-  const maxAmount = 1000000
-  const minAmount = 0.0004
+  const maxAmount = 3
+  const minAmount = 0.00004
   return NextResponse.json({ maxAmount: maxAmount, minAmount: minAmount })
 } 
